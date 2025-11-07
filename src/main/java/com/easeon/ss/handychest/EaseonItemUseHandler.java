aeef2468ee213c7107d21a8a6bbce54020546264
Teron <214763666+EaseonKit@users.noreply.github.com>
2025-11-08 00:33:09 +0900

    1.0.0.1

--- src/main/java/com/easeon/ss/handychest/EaseonItemUseHandler.java
+++ src/main/java/com/easeon/ss/handychest/EaseonItemUseHandler.java
@@ -1,6 +1,8 @@
 package com.easeon.ss.handychest;
 
 import com.easeon.ss.core.game.EaseonSound;
+import com.easeon.ss.core.wrapper.EaseonPlayer;
+import com.easeon.ss.core.wrapper.EaseonWorld;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,57 +13,59 @@ import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.ShulkerBoxScreenHandler;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class EaseonItemUseHandler {
-    public static ActionResult onUseItem(PlayerEntity player, World world, Hand hand) {
-        ItemStack itemStack = player.getStackInHand(hand);
+    public static ActionResult onUseItem(ServerPlayerEntity mcPlayer, World mcWorld, Hand hand) {
+        var world = new EaseonWorld(mcWorld);
+        var player = new EaseonPlayer(mcPlayer);
+        var item = player.getStackInHand(hand);
 
-        if (itemStack.getItem() == Items.ENDER_CHEST) {
-            var enderChest = player.getEnderChestInventory();
-            enderChest.onOpen(player);
+        if (item.of(Items.ENDER_CHEST)) {
+            var enderChest = player.getEnderChest();
+            enderChest.onOpen(player.get());
 
             player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, p) -> {
-                GenericContainerScreenHandler handler = GenericContainerScreenHandler.createGeneric9x3(syncId, inv, enderChest);
-                return new GenericContainerScreenHandler(handler.getType(), syncId, inv, enderChest, 3) {
+                    var handler = GenericContainerScreenHandler.createGeneric9x3(syncId, inv, enderChest);
+                    return new GenericContainerScreenHandler(handler.getType(), syncId, inv, enderChest, 3) {
+                        @Override
+                        public void onClosed(PlayerEntity player) {
+                            super.onClosed(player);
+                            enderChest.onClose(player);
+                            EaseonSound.play(player, SoundEvents.BLOCK_ENDER_CHEST_CLOSE);
+                        }
+                    };
+                }, item.getName()
+            ));
+
+            player.incrementStat(Stats.OPEN_ENDERCHEST);
+            player.swingHand(hand);
+            world.playSound(player, SoundEvents.BLOCK_ENDER_CHEST_OPEN, SoundCategory.BLOCKS, 0.5f);
+
+            return ActionResult.SUCCESS;
+        } else if (item.isIn(ItemTags.SHULKER_BOXES)) {
+            var inventory = getShulkerBoxInventory(item.get());
+            player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, inv, p) ->
+                new ShulkerBoxScreenHandler(syncId, inv, inventory) {
                     @Override
                     public void onClosed(PlayerEntity player) {
                         super.onClosed(player);
-                        enderChest.onClose(player);
-                        EaseonSound.play(player, SoundEvents.BLOCK_ENDER_CHEST_CLOSE);
+                        EaseonSound.play(player, SoundEvents.BLOCK_SHULKER_BOX_CLOSE);
+                        saveShulkerBoxInventory(item.get(), inventory);
                     }
-                };
-            }, Text.translatable("container.enderchest")));
-
-            player.swingHand(hand, true);
-            EaseonSound.play(player, SoundEvents.BLOCK_ENDER_CHEST_OPEN);
-
-            return ActionResult.SUCCESS;
-        } else if (itemStack.isIn(ItemTags.SHULKER_BOXES)) {
-            SimpleInventory inventory = getShulkerBoxInventory(itemStack);
-
-            player.openHandledScreen(new SimpleNamedScreenHandlerFactory(
-                    (syncId, inv, p) -> {
-                        return new ShulkerBoxScreenHandler(syncId, inv, inventory) {
-                            @Override
-                            public void onClosed(PlayerEntity player) {
-                                EaseonSound.play(player, SoundEvents.BLOCK_SHULKER_BOX_CLOSE);
-                                super.onClosed(player);
-                                saveShulkerBoxInventory(itemStack, inventory);
-                            }
-                        };
-                    },
-                    itemStack.getName()
+                },
+                item.getName()
             ));
 
             player.incrementStat(Stats.OPEN_SHULKER_BOX);
-            player.swingHand(hand, true);
-            EaseonSound.play(player, SoundEvents.BLOCK_SHULKER_BOX_OPEN);
+            player.swingHand(hand);
+            world.playSound(player, SoundEvents.BLOCK_SHULKER_BOX_OPEN, SoundCategory.BLOCKS, 0.5f);
 
             return ActionResult.SUCCESS;
         }
